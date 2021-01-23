4627751550331af4237dec9e11200d15198556ca
hszzz <hszzz.me@gmail.com>
2021-01-23 23:10:52 +0800

    add read fat12 fs

--- boot.asm
+++ boot.asm
@@ -1,31 +1,119 @@
 org 0x7c00
 
-start:			;initialize register value
-	mov ax, cs
-	mov ss, ax
-	mov ds, ax
-	mov es, ax
+jmp short start
+nop
 
-	mov si, msg
+define:
+    BaseOfStack equ 0x7c00
 
-print:			;print character in msg one by one
-	mov al, [si]	;fetch the address of the register si, which is the first byte of msg
-	add si, 1
-	cmp al, 0x00
-	je last
-	mov ah, 0x0e
-	mov bx, 0x0f
-	int 0x10
-	jmp print
+header:
+    BS_OEMName     db "hszzz"
+    BPB_BytsPerSec dw 512
+    BPB_SecPerClus db 1
+    BPB_RsvdSecCnt dw 1
+    BPB_NumFATs    db 2
+    BPB_RootEntCnt dw 224
+    BPB_TotSec16   dw 2880
+    BPB_Media      db 0xF0
+    BPB_FATSz16    dw 9
+    BPB_SecPerTrk  dw 18
+    BPB_NumHeads   dw 2
+    BPB_HiddSec    dd 0
+    BPB_TotSec32   dd 0
+    BS_DrvNum      db 0
+    BS_Reserved1   db 0
+    BS_BootSig     db 0x29
+    BS_VolID       dd 0
+    BS_VolLab      db "TOY-OS-0.01"
+    BS_FileSysType db "FAT12   "
 
-last:			;CPU stop
-	hlt
-	jmp last
+start:
+    mov ax, cs
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov sp, BaseOfStack
+    
+    mov ax, 34
+    mov cx, 1
+    mov bx, Buf
+    
+    call ReadSector
+    
+    mov bp, Buf
+    mov cx, 29
+    
+    call Print
+    
+last:
+    hlt
+    jmp last    
 
-msg:
-	db 0x0a, 0x0a	;0x0a = '\n'
-	db "hello toy-os"
-	db 0x0a, 0x0a
-	times 510-($-$$) db 0x00
-	db 0x55, 0xaa
+; es:bp --> string address
+; cx    --> string length
+Print:
+    mov ax, 0x1301
+    mov bx, 0x0007
+    int 0x10
+    ret
+
+; no parameter
+ResetFloppy:
+    push ax
+    push dx
+    
+    mov ah, 0x00
+    mov dl, [BS_DrvNum]
+    int 0x13
+    
+    pop dx
+    pop ax
+    
+    ret
+
+; ax    --> logic sector number
+; cx    --> number of sector
+; es:bx --> target address
+ReadSector:
+    push bx
+    push cx
+    push dx
+    push ax
+    
+    call ResetFloppy
+    
+    push bx
+    push cx
+    
+    mov bl, [BPB_SecPerTrk]
+    div bl
+    mov cl, ah
+    add cl, 1
+    mov ch, al
+    shr ch, 1
+    mov dh, al
+    and dh, 1
+    mov dl, [BS_DrvNum]
+    
+    pop ax
+    pop bx
+    
+    mov ah, 0x02
+
+read:    
+    int 0x13
+    jc read
+    
+    pop ax
+    pop dx
+    pop cx
+    pop bx
+    
+    ret
+
+MsgStr db  "Hello, TOY-OS!"    
+MsgLen equ ($-MsgStr)
+Buf:
+    times 510-($-$$) db 0x00
+    db 0x55, 0xaa
 
