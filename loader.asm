c63fd641452370b46759ed6a66e16432a93bccc4
hszzz <hszzz.me@gmail.com>
2021-01-26 21:38:19 +0800

    modify print string

--- loader.asm
+++ loader.asm
@@ -1,25 +1,24 @@
 org 0x9000
 
 begin:
-	mov si, msg
+    mov si, msg
 
 print:
-	mov al, [si]
-	add si, 1
-	cmp al, 0x00
-	je end
-	mov ah, 0x0e
-	mov bx, 0x0f
-	int 0x10
-	jmp print
+    mov al, [si]
+    add si, 1
+    cmp al, 0x00
+    je end
+    mov ah, 0x0E
+    mov bx, 0x0F
+    int 0x10
+    jmp print
 
 end:
-	hlt
-	jmp end
-
+    hlt
+    jmp end
+    
 msg:
-	db 0x0a, 0x0a
-	db "hello os!"
-	db 0x0a, 0x0a
-	db 0x00
-
+    db 0x0a, 0x0a
+    db "hello, toy-OS!"
+    db 0x0a, 0x0a
+    db 0x00
