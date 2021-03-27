b5417385c347c01cc103c68bb158ee980c1fc3cc
hszzz <hszzz.me@gmail.com>
2021-03-27 16:30:08 +0800

    modify loader.asm

--- loader.asm
+++ loader.asm
@@ -4,27 +4,31 @@ org 0x9000
 %include "common.asm"
 
 BaseOfStack  equ 0x9000
-BaseOfTarget equ 0xB000
+BaseOfTarget equ 0xC000
 Target       db  "KERNEL     "
-TarLen       db  $ - Target
+TarLen       equ $-Target
 
 [section .gdt]
-; GDT Definition BEGIN
-GDT_ENTRY     : Descriptor  0,       0,                    0
-CODE32_FLAT_DESC : Descriptor 0,     0xFFFFF,              DA_C + DA_32
-CODE32_DESC   : Descriptor  0,       Code32SegmentLen - 1, DA_C + DA_32
-; GDT Definition END
+; GDT definition
+GDT_ENTRY            :     Descriptor    0,    0,                    0
+CODE32_FLAT_DESC     :     Descriptor    0,    0xFFFFF,              DA_C + DA_32
+CODE32_DESC          :     Descriptor    0,    Code32SegmentLen - 1, DA_C + DA_32
+; GDT end
 
-GDT_LEN    equ    $ - GDT_ENTRY
+GdtLen    equ   $ - GDT_ENTRY
 
 GDT_PTR:
-    dw GDT_LEN	- 1
-    dd 0
+          dw   GdtLen - 1
+          dd   0
+          
+          
+; GDT Selector
+Code32FlatSelector    equ (0x0001 << 3) + SA_TIG + SA_RPL0
+Code32Selector        equ (0x0002 << 3) + SA_TIG + SA_RPL0
 
-; GDT Selector BEGIN
-Code32FlatSelector    equ    (0x0001 << 3) + SA_TIG + SA_RPL0
-Code32Selector        equ    (0x0002 << 3) + SA_TIG + SA_RPL0
-; GDT Selector END
+
+; end of [section .gdt]
+
 
 [section .s16]
 [bits 16]
@@ -34,63 +38,61 @@ BLMain:
     mov es, ax
     mov ss, ax
     mov sp, SPInitValue
-
+    
     ; initialize GDT for 32 bits code segment
     mov esi, CODE32_SEGMENT
     mov edi, CODE32_DESC
-    
     call InitDescItem
-
-
+    
     ; initialize GDT pointer struct
     mov eax, 0
     mov ax, ds
     shl eax, 4
     add eax, GDT_ENTRY
     mov dword [GDT_PTR + 2], eax
-
+    
     call LoadTarget
+	
+	cmp dx, 0
+	jz output
 
-    cmp dx, 0
-    jz output
-
-    ; load GDT
+    ; 1. load GDT
     lgdt [GDT_PTR]
-
-    ; close interrupt
-	cli
-
-    ; set IOPL to 3
+    
+    ; 2. close interrupt
+    ;    set IOPL to 3
+    cli 
+    
     pushf
     pop eax
-   
+    
     or eax, 0x3000
     
     push eax
     popf
-
-    ; open A20
+    
+    ; 3. open A20
     in al, 0x92
     or al, 00000010b
     out 0x92, al
-
-    ; enter protected mode
+    
+    ; 4. enter protect mode
     mov eax, cr0
     or eax, 0x01
     mov cr0, eax
-
-    ; jmp to 32 bits code segment
+    
+    ; 5. jump to 32 bits code
     jmp dword Code32Selector : 0
 
 output:	
     mov bp, Error
     mov cx, ErrLen
 	call Print
-				
+	
 	jmp $
 
-; esi  --> code segment 
-; edi  --> descriptor
+; esi --> code segment label
+; edi --> descriptor label
 InitDescItem:
     push eax
 
@@ -102,11 +104,11 @@ InitDescItem:
     shr eax, 16
     mov byte [edi + 4], al
     mov byte [edi + 7], ah
-
+    
     pop eax
-
-	ret
-
+    
+    ret
+    
 [section .s32]
 [bits 32]
 CODE32_SEGMENT:
@@ -114,8 +116,7 @@ CODE32_SEGMENT:
 
 Code32SegmentLen    equ    $ - CODE32_SEGMENT
 
-Error  db  "No KERNEL"	
-ErrLen equ $ - Error
+Error db  "No KERNEL"	
+ErrLen equ $-Error
 
 Buffer db  0
-
