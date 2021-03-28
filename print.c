6c38495288ade7ac4bd9384ecdec6577ab44e88b
hszzz <hszzz.me@gmail.com>
2021-03-28 16:39:31 +0800

    add print function in kernel

---
+++ print.c
@@ -0,0 +1,141 @@
+#include "print.h"
+
+static int printPosW = 0;
+static int printPosH = 0;
+static int printColor = PRINT_WHITE;
+
+int setPrintPosition(int w, int h)
+{
+	int ret = 0;
+
+	ret = ((0 <= w) && (w <= SCREEN_WIDTH) && (0 <= h) && (h <= SCREEN_HEIGHT));
+	if (ret)
+	{
+		printPosW = w;
+		printPosH = h;
+	}
+
+	return ret;
+}
+
+void setPrintColor(PrintColor color) 
+{
+	printColor = color;
+}
+
+void clearScreen()
+{
+	int w = 0;
+	int h = 0;
+	setPrintPosition(0, 0);
+
+	for (w=0; w<SCREEN_WIDTH; w++)
+	{
+		for (h=0; h<SCREEN_HEIGHT; h++)
+		{
+			printChar(' ');
+		}
+	}
+
+	setPrintPosition(0, 0);
+}
+
+int printChar(char c)
+{
+	int ret;
+
+	if ((c == '\n') || (c == '\r'))
+	{
+		ret = setPrintPosition(0, printPosH + 1);
+	}
+	else
+	{
+		int w = printPosW;
+		int h = printPosH;
+
+		if ((0 <= w) && (w <= SCREEN_WIDTH) && (0 <= h) && (h <= SCREEN_HEIGHT))
+		{
+			int edi = (SCREEN_WIDTH * h + w) * 2; 
+			char ah = printColor;
+			char al = c;
+
+			asm volatile
+			(
+				"movl %0, %%edi\n"
+				"movb %1, %%ah\n"
+				"movb %2, %%al\n"
+				"movw %%ax, %%gs:(%%edi)\n"
+				:
+				: "r"(edi), "r"(ah), "r"(al)
+				: "ax", "edi"
+			);
+
+			w++;
+
+			if (w == SCREEN_WIDTH)
+			{
+				w = 0;
+				h = h + 1;
+			}
+
+			ret = 1;
+		}
+
+		setPrintPosition(w, h);
+	}
+
+	return ret;
+}
+
+int printString(const char* s)
+{
+	int ret = 0;
+
+	if (s != NULL)
+	{
+		while (*s)
+		{
+			ret += printChar(*s++);
+		}
+	}
+	else
+	{
+		ret = -1;
+	}
+
+	return ret;
+}
+
+int printInt16(int i)
+{
+	int ret = 0;
+
+	return ret;
+}
+
+int printInt10(int n)
+{
+	int ret = 0;
+
+	if (n < 0) 
+	{
+		printChar('-');
+		n = -n;
+		ret += printInt10(n);
+	}
+	else
+	{
+		if (n < 10)
+		{
+			ret += printChar('0' + n);
+		}
+		else
+		{
+			printInt10(n / 10);
+			printInt10(n % 10);
+		}
+	}
+
+	return ret;
+}
+
---
+++ print.h
@@ -0,0 +1,30 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#define NULL ((void*)0)
+
+#define SCREEN_WIDTH  80
+#define SCREEN_HEIGHT 25
+
+typedef enum
+{
+	PRINT_GRAY   = 0x07,
+	PRINT_BLUE   = 0x09,
+	PRINT_GREEN  = 0x0A,
+	PRINT_RED    = 0x0C,
+	PRINT_YELLOW = 0x0E,
+	PRINT_WHITE  = 0x0F
+} PrintColor;
+
+int setPosition(int w, int h);
+void setPrintColor(PrintColor);
+
+void clearScreen();
+
+int printChar(char);
+int printString();
+int printInt10();
+int printInt16();
+
+#endif
+
