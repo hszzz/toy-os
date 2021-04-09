586c7802a05535133899f559c8bdf3c6846a9500
hszzz <hszzz.me@gmail.com>
2021-04-09 21:12:30 +0800

    feature: add getDescValue and setDescValue when kernel is running

---
+++ kernel/kernel.c
@@ -0,0 +1,33 @@
+#include "kernel.h"
+
+int setDescValue(Descriptor* desc, uint base, uint limit, ushort attr)
+{
+	int ret = 0;
+
+	if (ret = (desc == NULL))
+	{
+		desc->limit1       = limit & 0xFFFF;
+		desc->base1        = base & 0xFFFF;
+		desc->base2        = (base >> 16) & 0xFF;
+		desc->attr1        = attr & 0xFF;
+		desc->attr2_limit2 = ((attr >> 8) & 0xF0) | ((limit >> 16) & 0xF);
+		desc->base3        = (base >> 24) & 0xFF;
+	}
+
+	return ret;
+}
+
+int getDescValue(Descriptor* desc, uint* base, uint* limit, ushort* attr)
+{
+	int ret = 0;
+
+	if (ret = (desc && base && limit && attr))
+	{
+		*base  = (desc->base3 << 24) | (desc->base2 << 16) | desc->base1;
+		*limit = ((desc->attr2_limit2 & 0xF) << 16) | desc->limit1;
+		*attr  = ((desc->attr2_limit2 & 0xF0) << 8) | desc->attr1;
+	}
+
+	return ret;
+}
+
--- kernel/kernel.h
+++ kernel/kernel.h
@@ -1,7 +1,71 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#include "types.h"
+#include "const.h"
 
+typedef struct
+{
+	ushort limit1;
+	ushort base1;
+	byte   base2;
+	byte   attr1;
+	byte   attr2_limit2;
+	byte   base3;
+} Descriptor;
+
+typedef struct
+{
+	Descriptor* const entry;
+	const int         size;
+} gdtInfo;
+
+typedef struct
+{
+	uint gs;
+	uint fs;
+	uint es;
+	uint ds;
+	uint edi;
+	uint esi;
+	uint ebp;
+	uint kesp;
+	uint ebx;
+	uint edx;
+	uint ecx;
+	uint eax;
+	uint raddr;
+	uint eip;
+	uint cs;
+	uint eflags;
+	uint esp;
+	uint ss;
+} regValue;
+
+typedef struct 
+{
+	uint   previous;
+	uint   esp0;
+	uint   ss0;
+	uint   unused[22];
+	ushort reserved;
+	ushort iomb;
+} TSS;
+
+typedef struct
+{
+	regValue   rv;
+	Descriptor ldt[3];
+	TSS        tss;
+	ushort     ldtSelectot;
+	ushort     tssSelectot;
+	uint       id;
+	char       name[8];
+	byte       stack[512];
+} Process;
+
+int setDescValue(Descriptor* desc, uint base, uint limit, ushort attr);
+int getDescValue(Descriptor* desc, uint* base, uint* limit, ushort* attr);
 
 #endif
 
