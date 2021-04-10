335e20c28bc392fb82168178820a86357f2d6682
hszzz <hszzz.me@gmail.com>
2021-04-10 14:38:06 +0800

    feature: add InitGdt, kernel can load GDT to shared memory

--- kernel/kentry.asm
+++ kernel/kentry.asm
@@ -3,15 +3,28 @@
 global _start
 
 extern KMain
+extern gGdtInfo
 extern clearScreen
 
 [section .text]
 [bits 32]
 _start:
-    mov ebp, 0
+    mov ebp, 0 
+    call InitGdt
     call clearScreen
     call KMain
     
     jmp $
 
-
+InitGdt:
+    push ebp
+    mov ebp, esp
+    
+    mov eax, dword [GdtEntry]
+    mov [gGdtInfo], eax
+    mov eax, dword [GdtSize]
+    mov [gGdtInfo + 4], eax
+    
+    leave
+    
+    ret
