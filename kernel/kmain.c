c0e0755c2c7549ec979ff8c68a4904d8b7270905
hszzz <hszzz.me@gmail.com>
2021-04-10 14:42:13 +0800

    test: test getDescValue, get GDT when kernel is running

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -3,11 +3,28 @@
 
 void KMain()
 {
-	printString("123");
-	printString("123");
-	printString("123");
-	printString("123");
-	printString("123");
-	printString("123");
-	printString("123");
+	printString("toy-os\n");
+    uint base = 0;
+    uint limit = 0;
+    ushort attr = 0;
+    int i = 0;
+    
+    printString("GDT Entry: ");
+    printInt16((uint)gGdtInfo.entry);
+    printChar('\n');
+    
+    for(i=0; i<gGdtInfo.size; i++)
+    {
+        getDescValue(gGdtInfo.entry + i, &base, &limit, &attr);
+    
+        printInt16(base);
+        printString("    ");
+    
+        printInt16(limit);
+        printString("    ");
+    
+        printInt16(attr);
+        printChar('\n');
+    }
+	
 }
