67b93adefb08d7988b332fde23b5dbe7024fa590
hszzz <hszzz.me@gmail.com>
2021-08-18 10:41:13 +0800

    test: add kernel list test

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -6,6 +6,109 @@
 #include "utility.h"
 #include "logo.h"
 
+#include "list.h"
+
+typedef struct
+{
+    int x;
+    int y;
+
+    struct ListHead head;
+} TestNode;
+
+void TestKernelList()
+{    TestNode node[3];
+    struct ListHead list;
+    for (int i=0; i<3; ++i)
+    {
+        node[i].x = i;
+        node[i].y = i;
+
+        node->head.prev = NULL;
+        node->head.next = NULL;
+    }
+
+    ListInit(&list);
+    PrintString("test list is empty: ");
+    PrintInt10(ListIsEmpty(&list));
+    PrintChar('\n');
+    for (int i=0; i<3; ++i)
+    {
+        ListAdd(&list, &node[i].head);
+    }
+
+    struct ListHead* pos;
+    /*
+    ListForEach(pos, &list)
+    {
+        TestNode* node = ListEntry(pos, TestNode, head);
+        PrintInt10(node->x);
+        PrintChar(' ');
+        PrintInt10(node->y);
+        PrintChar('\n');
+    }
+    */
+
+    TestNode node1[5];
+    for (int i=0; i<5; ++i)
+    {
+        node1[i].x = i;
+        node1[i].y = i;
+
+        node1[i].head.prev = NULL;
+        node1[i].head.next = NULL;
+
+        ListAddTail(&list, &node1[i].head);
+    }
+    /*
+    ListForEach(pos, &list)
+    {
+        TestNode* node = ListEntry(pos, TestNode, head);
+        PrintInt10(node->x);
+        PrintChar(' ');
+        PrintInt10(node->y);
+        PrintChar('\n');
+    }
+    */
+
+    PrintString("test list is last: ");
+    PrintInt10(ListIsLast(&list, &node1[3].head));
+    PrintChar(' ');
+    PrintInt10(ListIsLast(&list, &node1[4].head));
+    PrintChar('\n');
+    PrintString("test list is empty: ");
+    PrintInt10(ListIsEmpty(&list));
+    PrintChar('\n');
+
+    ListDel(&node1[4].head);
+    /*
+    ListForEach(pos, &list)
+    {
+        TestNode* node = ListEntry(pos, TestNode, head);
+        PrintInt10(node->x);
+        PrintChar(' ');
+        PrintInt10(node->y);
+        PrintChar('\n');
+    }
+    */
+
+    TestNode node2;
+    node2.x = 9;
+    node2.y = 9;
+    node2.head.prev = NULL;
+    node2.head.next = NULL;
+
+    ListReplace(&node1[3].head, &node2.head);
+    ListForEach(pos, &list)
+    {
+        TestNode* node = ListEntry(pos, TestNode, head);
+        PrintInt10(node->x);
+        PrintChar(' ');
+        PrintInt10(node->y);
+        PrintChar('\n');
+    }
+}
+
 void KMain()
 {
     PrintLogo();
@@ -25,9 +128,12 @@ void KMain()
     PrintString("IDT Size: ");
     PrintInt10((uint)gIdtInfo.size);
     PrintChar('\n');
-    
-    InitInterrupts();
-    InitTasks();
-    LaunchTask();
+
+    TestKernelList();
+
+    // InitInterrupts();
+    // InitTasks();
+    // LaunchTask();
+    while (1);
 }
 
