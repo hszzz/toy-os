fb035715c9a9c05db94b28263a2cd71f5a5d4ae1
hszzz <hszzz.me@gmail.com>
2021-08-26 22:29:30 +0800

    feat: soft interrupt entry

--- kernel/ihandler.c
+++ kernel/ihandler.c
@@ -2,14 +2,20 @@
 #include "task.h"
 #include "interrupt.h"
 
+#include "syscall.h"
+
 void TimerHandler()
 {
 	Schedule();
-
 	SendEOI(MASTER_EOI_PORT);
 }
 
-void SystemCallHandler()
+void SystemCallHandler(ushort ax) // __cdecl__
 {
-
+    SetPrintPosition(0, 16);
+    PrintString("enter soft interrupt.");
+    if (ax == 1)
+    {
+        _exit();
+    }
 }
--- kernel/ihandler.h
+++ kernel/ihandler.h
@@ -1,11 +1,13 @@
 #ifndef IHANDLER_H
 #define IHANDLER_H
 
+#include "types.h"
+
 extern void TimerHandlerEntry();
 void TimerHandler();
 
 extern void SystemCallHandlerEntry();
-void SystemCallHandler();
+void SystemCallHandler(ushort ax);
 
 #endif // IHANDLER_H
 
