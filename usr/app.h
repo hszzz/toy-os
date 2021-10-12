8f182e39ec7f6d4430515b7b94e677526477c2eb
hszzz <hszzz.me@gmail.com>
2021-10-12 17:15:36 +0800

    feat: add app module, create user task

--- usr/app.h
+++ usr/app.h
@@ -1,10 +1,16 @@
 #ifndef APP_H
 #define APP_H
 
+#include "types.h"
+
 struct Application
 {
-    char* name;
+    const char* name;
     void (*tentry)();
 };
+
+void InitAppModule();
+struct Application* GetAppInfo(uint index);
+uint GetAppNum();
 
 #endif // APP_H
