1f54e40a17dd1f8ef499c1fc0e273b9f86f3b8a7
hszzz <hszzz.me@gmail.com>
2021-10-12 15:34:55 +0800

    feat: remove unnecessary debug information

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -10,21 +10,21 @@ void KMain()
 {
     PrintLogo();
 
-    PrintString("GDT Entry: ");
-    PrintInt16((uint)gGdtInfo.entry);
-    PrintChar('\n');
+    // PrintString("GDT Entry: ");
+    // PrintInt16((uint)gGdtInfo.entry);
+    // PrintChar('\n');
     
-    PrintString("GDT Size: ");
-    PrintInt10((uint)gGdtInfo.size);
-    PrintChar('\n');
+    // PrintString("GDT Size: ");
+    // PrintInt10((uint)gGdtInfo.size);
+    // PrintChar('\n');
 
-    PrintString("IDT Entry: ");
-    PrintInt16((uint)gIdtInfo.entry);
-    PrintChar('\n');
+    // PrintString("IDT Entry: ");
+    // PrintInt16((uint)gIdtInfo.entry);
+    // PrintChar('\n');
     
-    PrintString("IDT Size: ");
-    PrintInt10((uint)gIdtInfo.size);
-    PrintChar('\n');
+    // PrintString("IDT Size: ");
+    // PrintInt10((uint)gIdtInfo.size);
+    // PrintChar('\n');
 
     InitInterrupts();
     InitTasks();
