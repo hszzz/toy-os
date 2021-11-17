aa43dc02a69c27919eba4b29b4780bd79f5e6602
hszzz <hszzz.me@gmail.com>
2021-11-17 15:47:03 +0800

    style: clean code

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -8,23 +8,22 @@ void (*InitAppModule)() = (void*)0x1F000;
 void KMain()
 {
     PrintLogo();
-    PrintInt16((int)InitAppModule);
 
-    // PrintString("GDT Entry: ");
-    // PrintInt16((uint)gGdtInfo.entry);
-    // PrintChar('\n');
+    PrintString("GDT Entry: ");
+    PrintInt16((uint)gGdtInfo.entry);
+    PrintChar('\n');
     
-    // PrintString("GDT Size: ");
-    // PrintInt10((uint)gGdtInfo.size);
-    // PrintChar('\n');
+    PrintString("GDT Size: ");
+    PrintInt10((uint)gGdtInfo.size);
+    PrintChar('\n');
 
-    // PrintString("IDT Entry: ");
-    // PrintInt16((uint)gIdtInfo.entry);
-    // PrintChar('\n');
+    PrintString("IDT Entry: ");
+    PrintInt16((uint)gIdtInfo.entry);
+    PrintChar('\n');
     
-    // PrintString("IDT Size: ");
-    // PrintInt10((uint)gIdtInfo.size);
-    // PrintChar('\n');
+    PrintString("IDT Size: ");
+    PrintInt10((uint)gIdtInfo.size);
+    PrintChar('\n');
 
     InitAppModule();
     InitTaskModule();
